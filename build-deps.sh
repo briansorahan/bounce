a5015abb8d413901d96a388ee4a17afd5360da78
Brian Sorahan <brian.sorahan@enverus.com>
2026-02-12 12:40:37 -0600

    Fix CI build: Copy generated config header for foonathan/memory
    The config_impl.hpp file is generated by CMake during build but needs
    to be in the include path for node-gyp to find it.

--- build-deps.sh
+++ build-deps.sh
@@ -10,8 +10,14 @@ cd build
 cmake .. -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
          -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF \
          -DFOONATHAN_MEMORY_BUILD_TESTS=OFF \
-         -DFOONATHAN_MEMORY_BUILD_TOOLS=OFF
+         -DFOONATHAN_MEMORY_BUILD_TOOLS=OFF \
+         -DCMAKE_INSTALL_PREFIX=../../..
 make -j4
+
+# Copy generated config header to include directory
+mkdir -p ../include/foonathan/memory/detail
+cp src/config_impl.hpp ../include/foonathan/memory/detail/
+
 cd ../../..
 
 echo "Dependencies built successfully"
